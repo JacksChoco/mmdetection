d6eb66dbf8d4c7391e46f12e096a9e6f6950780c
JacksChoco <ljs8860@toss.im>
2023-05-14 15:15:16 +0900

    semantic_head 주석

--- configs/htc/htc_r50_fpn_1x_coco.py
+++ configs/htc/htc_r50_fpn_1x_coco.py
@@ -10,17 +10,18 @@ model = dict(
             roi_layer=dict(type='RoIAlign', output_size=14, sampling_ratio=0),
             out_channels=256,
             featmap_strides=[8]),
-        semantic_head=dict(
-            type='FusedSemanticHead',
-            num_ins=5,
-            fusion_level=1,
-            seg_scale_factor=1 / 8,
-            num_convs=4,
-            in_channels=256,
-            conv_out_channels=256,
-            num_classes=183,
-            loss_seg=dict(
-                type='CrossEntropyLoss', ignore_index=255, loss_weight=0.2))))
+        # semantic_head=dict(
+        #     type='FusedSemanticHead',
+        #     num_ins=5,
+        #     fusion_level=1,
+        #     seg_scale_factor=1 / 8,
+        #     num_convs=4,
+        #     in_channels=256,
+        #     conv_out_channels=256,
+        #     num_classes=183,
+        #     loss_seg=dict(
+        #         type='CrossEntropyLoss', ignore_index=255, loss_weight=0.2))
+                ))
 
 train_pipeline = [
     dict(type='LoadImageFromFile', backend_args={{_base_.backend_args}}),
